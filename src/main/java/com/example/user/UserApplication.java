6bb3357fec7f84421fdeefbf4267aae86889afcd
VenuraHapugala <vv.hapugala@gmail.com>
2023-08-26 22:51:25 +0530

    User Application File Implementations

--- src/main/java/com/example/user/UserApplication.java
+++ src/main/java/com/example/user/UserApplication.java
@@ -1,13 +1,36 @@
 package com.example.user;
 
+import com.example.user.configs.AuthenticationService;
+import com.example.user.payload.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import static com.example.user.models.Role.ADMIN;
 
+/**
+ * Main application class for the User Management System.
+ */
 @SpringBootApplication
 public class UserApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@gmail.com")
+                    .password("12345")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
 
+        };
+    }
 }
